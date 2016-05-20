78fc76c905c74f669069d07110752efcdb436ca0
deepak <deepak.a.1996@gmail.com>
2016-05-20 10:52:07 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Deepak::Application.config.secret_token = 'a6c89cd8a536c9f961ee6eb58f637f0af19dbafeca7feda91f3751c191a9132bbb40f15dea8cc81fa2056e7904a1add5b283377497d0b596664f4286bf650b0b'
